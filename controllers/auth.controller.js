903349cb7594fd1bd666875569d9d4efeeb3fe5d
Deepak Sharma <code.deepak9899sharma@gmail.com>
2025-06-06 17:33:10 +0530

    completed signup and login using mongoDB

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,58 +1,41 @@
 import fs from "fs"
+import { User } from "../models/User.js"
 
 
-export function handleLogin(req, res) {
-    fs.readFile("./USERS_DATA.json", "utf-8", (err, data) => {
-        if (err) res.end(err)
+export async function handleLogin(req, res) {
+    const loginData = req.body//data from login form
 
-        const loginData = req.body//data from login form
-        const parsed_user_data = JSON.parse(data)//JSON user parsed data
+    if (loginData["email"] && loginData["password"]) {
+        // checking if data is present in json file
+        let foundData = await User.findOne({ "email": loginData["email"], "password": loginData["password"] })
 
-        if (loginData["email"] && loginData["password"]) {
-            // checking if data is present in json file
-            let foundData = parsed_user_data.filter((user) => user["email"] === loginData["email"] && user["password"] === loginData["password"])
-
-            if (foundData.length > 0) {
-                res.render("profile", { "user": foundData[0] })
-            } else {
-                res.json({
-                    msg: "No user Found with given email and pass"
-                })
-            }
-
+        if (foundData) {
+            res.render("profile", { "user": foundData })
         } else {
-            res.json({
-                msg: "ABSENCE! check the name and password"
-            })
+            res.json({ msg: "No user Found with given email and pass" })
         }
-    })
+
+    } else {
+        res.json({
+            msg: "ABSENCE! check the name and password"
+        })
+    }
 }
 
-export function handleSignup(req, res) {
-    fs.readFile("./USERS_DATA.json", "utf-8", (err, data) => {
-        if (err) return res.end(err);
+export async function handleSignup(req, res) {
+    const signUpData = req.body//data from login form
+    // checking if data is present in json file
+    if (signUpData["name"] && signUpData["email"] && signUpData["gender"] && signUpData["password"]) {
 
-        const signUpData = req.body//data from login form
-        const parsed_user_data = JSON.parse(data)//JSON user parsed data
+        const allUserDocument = await await User.find()
+        signUpData["_id"] = allUserDocument[allUserDocument.length - 1]["_id"] + 1
 
-        // checking if data is present in json file
-        if (signUpData["name"] && signUpData["email"] && signUpData["gender"] && signUpData["password"]) {
+        const newSignUser = await User.insertOne(signUpData)
+        res.redirect("/login")
 
-            // updating readed data
-            parsed_user_data.push({
-                "id": parsed_user_data[parsed_user_data.length - 1].id + 1,
-                ...signUpData
-            })
-
-            // writing the update into file
-            fs.writeFile("./USERS_DATA.json", JSON.stringify(parsed_user_data), (err) => res.end(err));
-            res.redirect("/login")
-
-        } else {
-            res.json({
-                msg: "ABSENCE! check the signup field is filled properly!!"
-            })
-        }
-    })
-    
+    } else {
+        res.json({
+            msg: "ABSENCE! check the signup field is filled properly!!"
+        })
+    }
 }
